e05dd0776f43290214d9647c85cebd57e4b433da
barder60 <kaderate.yamamoto@hotmail.fr>
2021-07-08 10:46:46 +0200

    feat(pom): add modules business Logic

---
+++ MembershipManager/src/test/java/com/example/membershipmanager/MembershipManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.membershipmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipManagerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
