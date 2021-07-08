b914245189a4427197c22a42ba9b655515901144
MasatakaIshii <masaradio78@gmail.com>
2021-07-08 11:11:58 +0200

    Prepare CertificateController
    Update pom files of parent and education manager

---
+++ EducationManager/src/main/java/com/example/educationmanager/CertificateController.java
@@ -0,0 +1,9 @@
+package com.example.educationmanager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/certificate")
+public class CertificateController {
+}
